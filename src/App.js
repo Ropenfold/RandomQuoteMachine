2113aa13591fce5a8899de1be179be9eb6f0fc1c
Bukola <bukola_jimoh@yahoo.co.uk>
2019-03-18 16:55:06 +0000

    fetch request on ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: []
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then(data => data.json())
+      .then(res => this.setState({ quotes: res.quotes }));
+    //console.log(quotes);
+  }
+
   render() {
+    //console.log(this.state.quotes[0]);
+    const random = this.state.quotes[
+      Math.floor(Math.random() * this.state.quotes.length)
+    ];
+    console.log(random);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.quotes.map(i => (
+          <div>
+            {i.quote} - by {i.author}
+          </div>
+        ))}
       </div>
     );
   }
